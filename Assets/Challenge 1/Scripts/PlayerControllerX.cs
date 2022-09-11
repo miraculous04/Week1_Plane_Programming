0fd9972bbd3c234926abe18fd1d32ed61ee807ab
miraculous04 <66511028+miraculous04@users.noreply.github.com>
2022-09-11 01:33:25 +0100

    plane Asset
    Asset

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed =  0.5f;
+    private float rotationSpeed = 80.0f;
+    private float verticalInput;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the users vertical and horizontal input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * horizontalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PropellerRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotate : MonoBehaviour
+{
+
+    private Vector3 propellerRotate = new Vector3 (0, 0, 5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make propeller Rotate along Z axis only
+        transform.Rotate(propellerRotate);
+    }
+}
